29a7602c5e53bde5f154e16d3528c5d158e9c761
ivosoaresalmeida <ijpsalmeida@gmail.com>
2023-08-30 23:37:59 +0100

    Class Position added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Welcome to Chess Game!");
+
+        Position position = new Position(3,5);
+        System.out.println(position);
     }
 }
